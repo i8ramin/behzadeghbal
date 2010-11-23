dfa9437c4612a1316db553aeb28353aef9887c8b
Ramin Bozorgzadeh <ramin@etsy.com>
2010-11-23 13:46:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Behzad::Application.config.secret_token = 'edb2400bf8b851a90c21efecf9e97940420f646e37ef8ef4b269313ffdc24487f4dcc6ef64c062aa8f0b4dd97bdc3738a81676dba0116d0fe0e1d7332c74ab01'
